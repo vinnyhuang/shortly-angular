fc5a76873bcdcc8341a43a560a7364c387d4ae64
vinnyhuang <vincent.vwh.huang@gmail.com>
2016-07-09 16:56:15 -0700

    Implemented search bar for links filter.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -6,6 +6,26 @@ angular.module('shortly.links', [])
 
   Links.getAll()
     .then(function(links) {
+      console.log('links', links);
       $scope.data.links = links;
     });
+
+  $scope.search = function(link) {
+    // if (!$scope.query || (item.brand.toLowerCase().indexOf($scope.query) != -1) || (item.model.toLowerCase().indexOf($scope.query.toLowerCase()) != -1) ){
+    if (!$scope.query || link.title.toLowerCase().indexOf($scope.query) !== -1
+                      || link.url.toLowerCase().indexOf($scope.query) !== -1
+                      || link.code.toLowerCase().indexOf($scope.query) !== -1) {
+      return true;
+    }
+    return false;
+  };
+
 });
+
+// angular.module('myFilters', [])
+//   .filter('titleurl', function() {
+//     return function(links) {
+//       return links.filter(function())
+//     }
+//   })
+
